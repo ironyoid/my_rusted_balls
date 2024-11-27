ee05fbaba3f3ebfa218e0608274af08a8229a099
ironyoid <weedcuper@gmail.com>
2024-11-27 23:47:17 +0100

    balance bouncing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 #![allow(dead_code, unused_variables)]
 use raylib::prelude::*;
-const G: f32 = 9.82;
+use std::time::SystemTime;
 trait ProcessObject {
-    fn process(&mut self, time: f32, w: u32, h: u32);
+    fn process(&mut self, time: f64, w: u32, h: u32);
     fn draw(&self, draw_handler: &mut RaylibDrawHandle);
     fn detect_collision(&self, w: u32, h: u32) -> bool;
 }
 
 impl ProcessObject for Ball {
-    fn process(&mut self, time: f32, w: u32, h: u32) {
-        self.speed += self.acel * time;
-        self.delta_y += self.speed * time + self.speed * time.powf(2.0) / 2.0;
-        self.y = self.delta_y.round() as u32;
+    fn process(&mut self, time: f64, w: u32, h: u32) {
+        self.speed = self.speed + self.acel * time;
+        self.total += self.acel * time;
+        self.y = self.y + self.speed * time + self.acel * time.powf(2.0) / 2.0;
+        // println!("y: {}", self.y);
         if self.detect_collision(w, h) {
-            self.acel = self.acel;
+            self.y = (h - self.radius) as f64;
             self.speed = -self.speed;
+            print!(
+                "speed: {} y: {} total: {}\n",
+                self.speed, self.y, self.total
+            );
+            self.total = 0.0;
         }
     }
     fn detect_collision(&self, w: u32, h: u32) -> bool {
-        if self.y < h - self.radius {
+        if self.y <= h as f64 {
             false
         } else {
             true
@@ -29,67 +35,30 @@ impl ProcessObject for Ball {
     }
 }
 
-// impl ProcessObject for Square {
-//     fn process(&mut self, time: f32, w: u32, h: u32) {
-//         print!("Square y={}\n", self.y);
-//     }
-//     fn draw(&self, draw_handler: &mut RaylibDrawHandle) {}
-// }
-
 struct Ball {
     init_x: u32,
     init_y: u32,
-    x: u32,
-    y: u32,
-    delta_y: f32,
+    x: f32,
+    y: f64,
     radius: u32,
-    speed: f32,
-    acel: f32,
+    speed: f64,
+    acel: f64,
     color: Color,
-    name: String,
+    total: f64,
 }
 
 impl Ball {
-    pub fn new(x: u32, y: u32, radius: u32, acel: f32, name: String) -> Self {
+    pub fn new(x: u32, y: u32, radius: u32, acel: f64, color: Color) -> Self {
         Self {
             init_x: x,
             init_y: y,
-            x: x,
-            y: y,
+            x: x as f32,
+            y: y as f64,
             acel: acel,
-            delta_y: y as f32,
             radius: radius,
             speed: 0.0,
-            color: Color::RED,
-            name: name,
-        }
-    }
-}
-
-struct Square {
-    init_x: u32,
-    init_y: u32,
-    w: u32,
-    h: u32,
-    x: u32,
-    y: u32,
-    speed: u32,
-    color: Color,
-    name: String,
-}
-
-impl Square {
-    pub fn new(x: u32, y: u32, w: u32, h: u32, name: String) -> Self {
-        Self {
-            init_x: x,
-            init_y: y,
-            x: x,
-            y: y,
-            w: w,
-            h: h,
-            speed: 0,
-            color: Color::RED,
-            name: name,
+            color: color,
+            total: 0.0,
         }
     }
 }
@@ -97,13 +66,20 @@ impl Square {
 struct Model {
     width: u32,
     height: u32,
+    last_time: u128,
     objects: Vec<Box<dyn ProcessObject>>,
 }
 
 impl Model {
     pub fn process(&mut self) {
-        for object in self.objects.iter_mut() {
-            object.process(0.001, self.width, self.height);
+        let curr_time = get_time_us();
+        let time_delta = (curr_time - self.last_time) as f64 / 1000000.0;
+        if time_delta >= 0.02 {
+            self.last_time = curr_time;
+            for object in self.objects.iter_mut() {
+                //print!("time_delta: {}\n", time_delta);
+                object.process(0.02, self.width, self.height);
+            }
         }
     }
     pub fn draw(&mut self, draw_handler: &mut RaylibDrawHandle) {
@@ -113,14 +89,27 @@ impl Model {
     }
 }
 
+fn get_time_us() -> u128 {
+    let duration_since_epoch = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    duration_since_epoch.as_micros()
+}
 fn main() {
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
-    let ball1 = Ball::new(320, 240, 20, G, String::from("Ball 1"));
-    let ball2 = Ball::new(400, 240, 20, 5.25, String::from("Ball 2"));
+    const G: f64 = 10000.0;
+    const WINDOW_WIDTH: u32 = 640;
+    const WINDOW_HEIGHT: u32 = 480;
+    let (mut rl, thread) = raylib::init()
+        .size(WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
+        .title("Bouncing Balls")
+        .build();
+    let ball1 = Ball::new(320, 240, 20, G, Color::RED);
+    let ball2 = Ball::new(400, 240, 20, G / 2.0, Color::BLUE);
     let mut model = Model {
-        width: 640,
-        height: 480,
-        objects: vec![Box::new(ball1), Box::new(ball2)],
+        width: WINDOW_WIDTH,
+        height: WINDOW_HEIGHT,
+        last_time: get_time_us(),
+        objects: vec![Box::new(ball1)],
     };
 
     while !rl.window_should_close() {
@@ -128,6 +117,5 @@ fn main() {
         d.clear_background(Color::WHITE);
         model.process();
         model.draw(&mut d);
-        //    d.draw_circle(ball.x as i32, ball.y as i32, ball.radius, ball.color);
     }
 }
